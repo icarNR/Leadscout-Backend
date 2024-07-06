cb813d8bfe4ea8c1dd63bdefc3bb1bce4b560370
Bhawa1018 <124970079+Bhawa1018@users.noreply.github.com>
2024-07-06 15:28:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.routes.otp import router as otp_router
 from app.routes.adminDashboard import router as adminDashboard_router
 from fastapi import FastAPI
 from crorSetting import setup_cors
+from routes.Employee_Criteria import router as employee_criteria_router
 
 # The rest of your FastAPI application code goes here
 
@@ -19,4 +20,5 @@ app.include_router(authentication_router)
 app.include_router(otp_router)
 app.include_router(adminDashboard_router)
 app.include_router(profile_router)
+app.include_router(employee_criteria_router)
 
